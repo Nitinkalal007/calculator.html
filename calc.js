7d55073685039b81f8f7ce251d5429999da1908c
Nitinkalal007 <nitinkalal01@gmail.com>
2025-05-19 11:17:13 +0530

    first commit

---
+++ calc.js
@@ -0,0 +1,16 @@
+function clearAll(){
+    document.querySelector('#res').value=""
+
+}
+function lastdelete(){
+    var invalue=document.querySelector('#res')
+    invalue.value=invalue.value.slice(0,-3)
+}
+function calc(num){
+    document.querySelector('#res').value+=num
+}
+function output(){
+    var invalue=document.querySelector('#res')
+    var finaloutput=eval(invalue.value)
+    invalue.value=finaloutput
+} 
