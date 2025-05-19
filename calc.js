6cd808f149f124a094626f0784d0ce5671406dcf
Nitinkalal007 <nitinkalal01@gmail.com>
2025-05-19 12:09:45 +0530

    update code

--- calc.js
+++ calc.js
@@ -4,7 +4,7 @@ function clearAll(){
 }
 function lastdelete(){
     var invalue=document.querySelector('#res')
-    invalue.value=invalue.value.slice(0,-3)
+    invalue.value=invalue.value.slice(0,-1)
 }
 function calc(num){
     document.querySelector('#res').value+=num
